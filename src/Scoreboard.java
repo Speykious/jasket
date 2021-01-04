2728bfc7400e15313eb68625a62ab650a906174c
Speykious <speykious@gmail.com>
2021-01-04 19:22:30 +0100

    Might as well make the final ints public to access them

--- src/Scoreboard.java
+++ src/Scoreboard.java
@@ -1,8 +1,8 @@
 public class Scoreboard {
   /** Longueur d'un scoreboard en caractères. */
-  private static final int width = 80;
+  public static final int width = 80;
   /** Hauteur d'un scoreboard en caractères. */
-  private static final int height = 20;
+  public static final int height = 20;
 
   /** Équipe "Guest". */
   public Team guest;
