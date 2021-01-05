31a340a9fb29e3ab2e8008ebff8242dc3b88ab5c
Speykious <speykious@gmail.com>
2021-01-05 22:11:36 +0100

    Crossover in the right place

--- src/Guard.java
+++ src/Guard.java
@@ -2,4 +2,15 @@ public abstract class Guard extends MovingPlayer {
   public Guard(String name) {
     super(name);
   }
+
+  /**
+   * crossover retourne quand le meneur a crossé (passer en dribblant) un joueur
+   * adverse
+   * 
+   * @param p Le joueur adverse
+   * @return String
+   */
+  public String crossover(Player p) {
+    return "Le joueur " + super.name + " a crossé le joueur " + p.getName() + " !";
+  }
 }
--- src/PointGuard.java
+++ src/PointGuard.java
@@ -7,33 +7,4 @@ public class PointGuard extends Guard {
     super(name);
   }
 
-  /**
-   * Prévient que le joueur attaque
-   * 
-   * @return Le message de notification
-   */
-  public String attack() {
-    return "le joueur " + super.name + " attaque !";
-  }
-
-  /**
-   * Prévient que le meneur défend sur le joueur adverse
-   * 
-   * @param p Le joueur adverse
-   * @return Le message de notification
-   */
-  public String defense(Player p) {
-    return "Le joueur " + super.name + " défend sur le joueur " + p.getName() + " !";
-  }
-
-  /**
-   * crossover retourne quand le meneur a crossé (passer en dribblant) un joueur
-   * adverse
-   * 
-   * @param p Le joueur adverse
-   * @return String
-   */
-  public String crossover(Player p) {
-    return "Le joueur " + super.name + " a crossé le joueur " + p.getName() + " !";
-  }
 }
