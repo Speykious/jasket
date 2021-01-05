085c95fa6145211903be06f5a0b30408676dc86a
Speykious <speykious@gmail.com>
2021-01-05 22:11:17 +0100

    Screen

--- src/Forward.java
+++ src/Forward.java
@@ -2,4 +2,8 @@ public abstract class Forward extends MovingPlayer {
   public Forward(String name) {
     super(name);
   }
+
+  public String screen(Guard g, Player p) {
+    return name + " a screen " + p.name + " grâce à " + g.name + " !";
+  }
 }
